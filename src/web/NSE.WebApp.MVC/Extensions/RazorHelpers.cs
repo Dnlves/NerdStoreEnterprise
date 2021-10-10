5f9ccc63a46ca6ebd09806beea8aa7fab0411a7e
Diogo Alves <dnalves52@gmail.com>
2021-10-10 14:03:16 -0300

    Layout do e-commerce

---
+++ src/web/NSE.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public static class RazorHelpers
+    {
+        public static string HashEmailForGravatar(this RazorPage page, string email)
+        {
+            var md5Hasher = MD5.Create();
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(email));
+            var sBuilder = new StringBuilder();
+            foreach (var t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
