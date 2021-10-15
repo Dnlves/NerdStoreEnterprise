8771bcacfca909bae054a7b418b2ee16dbf9e9d0
Diogo Alves <dnalves52@gmail.com>
2021-10-15 16:34:55 -0300

    Retry Pattern Com Polly

--- src/web/NSE.WebApp.MVC/Configuration/DependecyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependecyInjectionConfig.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -16,17 +18,35 @@ namespace NSE.WebApp.MVC.Configuration
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
             
-            // services
-            //     .AddHttpClient<ICatalogoService, CatalogoService>()
-            //     .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
-
+            var retryWaitPolicy = HttpPolicyExtensions
+                                    .HandleTransientHttpError()
+                                    .WaitAndRetryAsync(
+                                        sleepDurations: new [] { 
+                                            TimeSpan.FromSeconds(1),
+                                            TimeSpan.FromSeconds(5),
+                                            TimeSpan.FromSeconds(10),
+                                        },
+                                        onRetry: (outcome, TimeSpan, retryCount, context) => 
+                                        {
+                                            Console.ForegroundColor = ConsoleColor.Blue;
+                                            Console.WriteLine($"Tentando pela {retryCount} vez!");
+                                            Console.ForegroundColor = ConsoleColor.White;
+                                        }
+                                    );
+            
             services
-                .AddHttpClient(name: "Refit", configureClient: options => 
-                {
-                    options.BaseAddress = new Uri(configuration.GetSection(key: "CatalogoUrl").Value);
-                })
+                .AddHttpClient<ICatalogoService, CatalogoService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-                .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+                // .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+                .AddPolicyHandler(retryWaitPolicy);
+
+            // services
+            //     .AddHttpClient(name: "Refit", configureClient: options => 
+            //     {
+            //         options.BaseAddress = new Uri(configuration.GetSection(key: "CatalogoUrl").Value);
+            //     })
+            //     .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            //     .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
