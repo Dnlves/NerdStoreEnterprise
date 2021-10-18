60238bbdbcdbb1e38d1a4f9c8e28e70434f89a10
Diogo Alves <dnalves52@gmail.com>
2021-10-18 15:38:03 -0300

    Validação de comandos

--- src/services/NSE.Clientes.API/Application/Commands/RegistrarClienteCommand.cs
+++ src/services/NSE.Clientes.API/Application/Commands/RegistrarClienteCommand.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentValidation;
 using NSE.Core.Messages;
 
 namespace NSE.Clientes.API.Application.Commands
@@ -20,5 +21,37 @@ namespace NSE.Clientes.API.Application.Commands
             Email = email;
             Cpf = cpf;
         }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new RegistrarClienteValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+
+        // classe aninhada de validação
+        public class RegistrarClienteValidation : AbstractValidator<RegistrarClienteCommand>
+        {
+            public RegistrarClienteValidation()
+            {
+                RuleFor(c => c.Id).NotEqual(Guid.Empty).WithMessage("Id do cliente inválido");
+
+                RuleFor(c => c.Nome).NotEmpty().WithMessage("O nome do cliente não foi informado");
+
+                RuleFor(c => c.Email).Must(TerEmailValido).WithMessage("Email informado não é válido");
+
+                RuleFor(c => c.Cpf).Must(TerCpfValido).WithMessage("Cpf informado não é válido");
+            }
+
+            protected static bool TerEmailValido(string email)
+            {
+                return Core.DomainObjects.Email.Validar(email);
+            }
+
+            protected static bool TerCpfValido(string cpf)
+            {
+                return Core.DomainObjects.Cpf.Validar(cpf);
+            }
+        }
     }
+
 }
