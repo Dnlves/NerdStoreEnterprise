d752a670c5f4a32286eec471e36a0f9d8d0d3093
Diogo Alves <dnalves52@gmail.com>
2021-10-21 17:05:39 -0300

    Melhorando a resiliência da aplicação

--- src/building-blocks/NSE.MessageBus/MessageBus.cs
+++ src/building-blocks/NSE.MessageBus/MessageBus.cs
@@ -10,9 +10,11 @@ namespace NSE.MessageBus
     public class MessageBus : IMessageBus
     {
         private IBus _bus;
-        public bool IsConnected => _bus?.Advanced.IsConnected ?? false;
+        private IAdvancedBus _advancedBus;
         private readonly string _connectionString;
 
+        public bool IsConnected => _bus?.Advanced.IsConnected ?? false;
+        public IAdvancedBus AdvancedBus => _bus?.Advanced;
 
         public MessageBus(string connectionString)
         {
@@ -82,7 +84,24 @@ namespace NSE.MessageBus
                 .Or<BrokerUnreachableException>()
                 .WaitAndRetry(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
 
-            policy.Execute(() => {  _bus = RabbitHutch.CreateBus(_connectionString); });
+            policy.Execute(() => 
+            {  
+                _bus = RabbitHutch.CreateBus(_connectionString); 
+
+                _advancedBus = _bus.Advanced;
+
+                _advancedBus.Disconnected += OnDisconnect;
+            });
+        }
+
+        private void OnDisconnect(object s, EventArgs e)
+        {
+            var policy = Policy
+                .Handle<EasyNetQException>()
+                .Or<BrokerUnreachableException>()
+                .RetryForever();
+
+            policy.Execute(TryConnect);
         }
 
         public void Dispose()
