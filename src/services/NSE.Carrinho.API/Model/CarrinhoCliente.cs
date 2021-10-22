17f01f13a691eb902a6bd95c446740bf03c4a37c
Diogo Alves <dnalves52@gmail.com>
2021-10-22 18:25:53 -0300

    Comportamentos de negócio da API - pt I

--- src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NSE.Carrinho.API.Model
 {
     public class CarrinhoCliente
     {
+        internal const int MAX_QUANTIDADE_ITEM = 5;
 
         public Guid Id { get; set; }
         public Guid ClienteId { get; set; }
@@ -12,15 +14,50 @@ namespace NSE.Carrinho.API.Model
         public List<CarrinhoItem> Itens { get; set; } = new List<CarrinhoItem>();
         
 
-        public CarrinhoCliente(Guid id, Guid clienteId)
+        public CarrinhoCliente(Guid clienteId)
         {
-            Id = id;
+            Id = Guid.NewGuid();
             ClienteId = clienteId;
         }
         
         public CarrinhoCliente()
         {
-            
+        }
+
+
+        internal void CalcularValorCarrinho()
+        {
+            ValorTotal = Itens.Sum(p => p.CalcularValor());
+        }
+
+        internal bool CarrinhoItemExistente(CarrinhoItem item)
+        {
+            return Itens.Any(p => p.ProdutoId == item.ProdutoId);
+        }
+
+        internal CarrinhoItem ObterPorProdutoId(Guid produtoId)
+        {
+            return Itens.FirstOrDefault(p => p.ProdutoId == produtoId);
+        }
+
+
+        internal void AdicionarItem(CarrinhoItem item)
+        {
+            if (!item.EhValido()) return;
+
+            item.AdicionarItem(Id);
+
+            if (CarrinhoItemExistente(item))
+            {
+                var itemExistente = ObterPorProdutoId(item.ProdutoId);
+                itemExistente.AdicionarUnidades(item.Quantidade);
+
+                item = itemExistente;
+                Itens.Remove(itemExistente);
+            }
+
+            Itens.Add(item);
+            CalcularValorCarrinho();
         }
           
     }
