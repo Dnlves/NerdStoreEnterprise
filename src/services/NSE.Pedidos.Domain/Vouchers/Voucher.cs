779aec52346a05d2301a88c19c189b5e80af2d84
Diogo Alves <dnalves52@gmail.com>
2021-10-26 18:09:52 -0300

    Application Query Stack e Specification Pattern

--- src/services/NSE.Pedidos.Domain/Vouchers/Voucher.cs
+++ src/services/NSE.Pedidos.Domain/Vouchers/Voucher.cs
@@ -1,5 +1,6 @@
 using System;
 using NSE.Core.DomainObjects;
+using NSE.Pedidos.Domain.Vouchers.Specs;
 
 namespace NSE.Pedidos.Domain.Vouchers
 {
@@ -15,5 +16,29 @@ namespace NSE.Pedidos.Domain.Vouchers
         public DateTime DataValidade { get; private set; }
         public bool Ativo { get; private set; }
         public bool Utilizado { get; private set; }
+
+        public bool EstaValidoParaUtilizacao()
+        {
+            return new VoucherAtivoSpecification()
+                .And(new VoucherDataSpecification())
+                .And(new VoucherQuantidadeSpecification())
+                .IsSatisfiedBy(this);
+        }
+
+        public void MarcarComoUtilizado()
+        {
+            Ativo = false;
+            Utilizado = true;
+            Quantidade = 0;
+            DataUtilizacao = DateTime.Now;
+        }
+
+        public void DebitarQuantidade()
+        {
+            Quantidade -= 1;
+            if (Quantidade >= 1) return;
+
+            MarcarComoUtilizado();
+        }
     }
 }
