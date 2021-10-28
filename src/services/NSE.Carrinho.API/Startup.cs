001786cc5fb15e23c455f6ad35d02160ac9187c0
Diogo Alves <dnalves52@gmail.com>
2021-10-28 14:25:56 -0300

    Manipulando o carrinho via RabbitMQ

--- src/services/NSE.Carrinho.API/Startup.cs
+++ src/services/NSE.Carrinho.API/Startup.cs
@@ -38,6 +38,8 @@ namespace NSE.Carrinho.API
             services.AddSwaggerConfiguration();
 
             services.RegisterServices();
+
+            services.AddMessageBusConfiguration(Configuration);
         }
 
 
